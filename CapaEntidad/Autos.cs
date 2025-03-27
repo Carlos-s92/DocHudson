ab91026a73932b057c1d219632d724ee40721ae5
Corterwar <kevingaray2005@gmail.com>
2025-03-27 17:38:05 -0300

    Se agregaron las entidades a la capa entidad

---
+++ CapaEntidad/Autos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Autos
+    {
+        public int Id_Auto { get; set; }
+        public bool Estado { get; set; }
+        public String Modelo { get; set; }
+
+        public String Matricula { get; set; }       
+        public decimal Kilometros { get; set; }
+        public int Año { get; set; }
+        public decimal Consumo { get; set; }
+
+        public DateTime Fecha_Registro { get; set; }
+    }
+}
---
+++ CapaEntidad/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Usuarios
+    {
+        public int Id_Usuario { get; set; }
+
+        public Perfiles oPerfil { get; set; }
+        public string Usuario { get; set; }
+
+        public string Contraseña { get; set; }
+
+        public DateTime Fecha_Registro { get; set; }
+
+    }
+}
